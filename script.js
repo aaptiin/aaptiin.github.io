bff32fb4ed74472616770b4796c68d2893e86f79
piebytwo <shivamsonidev@gmail.com>
2021-08-10 13:32:08 +0530

    adds mobile responsiveness

---
+++ script.js
@@ -0,0 +1,48 @@
+$(document).ready(function () {
+    var swiperA = new Swiper('.work .swiper-container', {
+        slidesPerView: 1.3,
+        spaceBetween: 0,
+        grabCursor: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: '.swiper-pagination',
+        },
+        breakpoints: {
+            768: {
+                slidesPerView: 2.05,
+                spaceBetween: 30
+            },
+            1024: {
+                slidesPerView: 4.05,
+                spaceBetween: 30
+            }
+        },
+    });
+    var swiperB = new Swiper('.researchcards .swiper-container', {
+        slidesPerView: 1.2,
+        spaceBetween: 0,
+        grabCursor: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: '.swiper-pagination',
+        }
+    });
+    var swiperC = new Swiper('.aimcards .swiper-container', {
+        slidesPerView: 1.2,
+        spaceBetween: 0,
+        grabCursor: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: '.swiper-pagination',
+        }
+    });
+    var swiperD = new Swiper('.howaimcards .swiper-container', {
+        slidesPerView: 1.2,
+        spaceBetween: 0,
+        grabCursor: true,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: '.swiper-pagination',
+        }
+    });
+});
